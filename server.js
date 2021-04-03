3aca0aa02205628c29dd6554deb24a6130d3789f
Rishabh Agrawal <ragrawal804@gmail.com>
2021-04-04 02:50:35 +0530

    change basic text

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/',(req,res)=>{
-    res.send('[This is sample file]');
+    res.send('[This text is after some change]');
 });
 
 app.listen(3000,()=>{console.log(`[Application is listening on port ${PORT}]`)});
