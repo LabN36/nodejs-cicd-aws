66873867cfd34eb23121ad79d27b6decdfa272e6
Rishabh Agrawal <ragrawal804@gmail.com>
2021-04-04 06:09:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/',(req,res)=>{
-    res.send('[Permission Working Timeout v6.0.1]');
+    res.send('[Changed this file from server side v4.0]');
 });
 
 app.listen(3000,()=>{console.log(`[Application is listening on port ${PORT}]`)});
